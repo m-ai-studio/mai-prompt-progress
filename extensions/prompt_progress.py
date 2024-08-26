57508720c9f65def13215ee3a302a7687b5d888e
Josef Duda <josefduda@gmail.com>
2024-08-26 18:02:51 +0200

    feat: send preview progress image

--- extensions/prompt_progress.py
+++ extensions/prompt_progress.py
@@ -17,14 +17,13 @@ class PromptEvent:
 
 
 class DataKeys:
-    PROMPT_ID = "prompt_id"
+    CLIENT_ID = "client_id"
     NODE = "node"
     VALUE = "value"
     MAX = "max"
 
 
 class PromptKeys:
-    SID = "sid"
     CURRENT_NODE = "current_node"
     NODES = "nodes"
     NODE_PROGRESS = "node_progress"
@@ -39,70 +38,62 @@ prompts_map = {}
 # ===========================================================
 
 
-def handle_execution_start(data, sid):
-    prompt_id = data.get(DataKeys.PROMPT_ID)
-    if prompt_id:
-        prompt_queue = server.PromptServer.instance.prompt_queue
-        for prompt_data in prompt_queue.currently_running.values():
-            if prompt_data[1] == prompt_id:
-                nodes = prompt_data[2].keys()
-                prompts_map[prompt_id] = {
-                    PromptKeys.SID: sid,
-                    PromptKeys.CURRENT_NODE: None,
-                    PromptKeys.NODES: {
-                        node: {PromptKeys.NODE_PROGRESS: 0} for node in nodes
-                    },
-                    PromptKeys.HOOK_URL: prompt_data[3].get("progress_hook_url"),
-                }
-                break
+def handle_execution_start(sid):
+    prompt_queue = server.PromptServer.instance.prompt_queue
+    for prompt_data in prompt_queue.currently_running.values():
+        if prompt_data[3].get(DataKeys.CLIENT_ID) == sid:
+            nodes = prompt_data[2].keys()
+            prompts_map[sid] = {
+                PromptKeys.CURRENT_NODE: None,
+                PromptKeys.NODES: {
+                    node: {PromptKeys.NODE_PROGRESS: 0} for node in nodes
+                },
+                PromptKeys.HOOK_URL: prompt_data[3].get("progress_hook_url"),
+            }
+            break
 
 
-def handle_execution_cached(data):
+def handle_execution_cached(data, sid):
     # Remove cached nodes from the map
-    prompt_id = data.get(DataKeys.PROMPT_ID)
     nodes = data.get(PromptKeys.NODES, [])
-    map_data = prompts_map.get(prompt_id, {})
+    map_data = prompts_map.get(sid, {})
 
     for node_id in nodes:
         map_data.get(PromptKeys.NODES, {}).pop(node_id, None)
 
 
-def handle_executing(data):
-    prompt_id = data.get(DataKeys.PROMPT_ID)
+def handle_executing(data, sid):
     current_node_id = data.get(DataKeys.NODE)
-    map_data = prompts_map.get(prompt_id)
+    map_data = prompts_map.get(sid)
     if map_data:
         previous_node_id = map_data.get(PromptKeys.CURRENT_NODE)
-        prompts_map[prompt_id][PromptKeys.CURRENT_NODE] = current_node_id
+        prompts_map[sid][PromptKeys.CURRENT_NODE] = current_node_id
         if current_node_id != previous_node_id:
-            update_node_progress(prompt_id, previous_node_id, 1)
+            update_node_progress(sid, previous_node_id, 1)
 
 
-def handle_progress(data):
-    prompt_id = data.get(DataKeys.PROMPT_ID)
+def handle_progress(data, sid):
     node_id = data.get(DataKeys.NODE)
     value = data.get(DataKeys.VALUE)
     max_value = data.get(DataKeys.MAX)
-    if prompt_id and node_id and value is not None and max_value is not None:
-        update_node_progress(prompt_id, node_id, value / max_value)
+    if node_id and value is not None and max_value is not None:
+        update_node_progress(sid, node_id, value / max_value)
 
 
-def handle_executed(data):
+def handle_executed(data, sid):
     if data.get("output", {}).get("images", [{}])[0].get("type") == "output":
-        prompts_map.pop(data.get(DataKeys.PROMPT_ID), None)
+        prompts_map.pop(sid, None)
 
 
-def update_node_progress(prompt_id, node_id, value):
-    node_data = prompts_map.get(prompt_id, {}).get(PromptKeys.NODES, {}).get(node_id)
+def update_node_progress(sid, node_id, value):
+    node_data = prompts_map.get(sid, {}).get(PromptKeys.NODES, {}).get(node_id)
     if node_data:
-        prompts_map[prompt_id][PromptKeys.NODES][node_id][
-            PromptKeys.NODE_PROGRESS
-        ] = value
-        progress_updated(prompt_id)
+        prompts_map[sid][PromptKeys.NODES][node_id][PromptKeys.NODE_PROGRESS] = value
+        progress_updated(sid)
 
 
-def progress_updated(prompt_id):
-    map_data = prompts_map.get(prompt_id)
+def progress_updated(sid):
+    map_data = prompts_map.get(sid)
     if map_data:
         total_nodes = len(map_data.get(PromptKeys.NODES))
         progress = (
@@ -112,21 +103,32 @@ def progress_updated(prompt_id):
             )
             / total_nodes
         )
-        send_progress_update(prompt_id, progress)
+        send_progress_update(sid, progress)
 
 
-def send_progress_update(prompt_id, value):
+def send_progress_update(sid, value):
     # Debug info
     percentage = round(value * 100, 1)
     print(f"Progress: {percentage}%")
 
     # Send progress update to hook
-    sid = prompts_map.get(prompt_id, {}).get(PromptKeys.SID)
-    hook_url = prompts_map.get(prompt_id, {}).get(PromptKeys.HOOK_URL)
+    hook_url = prompts_map.get(sid, {}).get(PromptKeys.HOOK_URL)
 
     if sid and hook_url:
         try:
-            response = requests.post(hook_url, json={"value": value, "sid": sid})
+            response = requests.post(hook_url, json={"sid": sid, "value": value})
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logging.error(f"Progress extension hook request error: {e}")
+
+
+def send_preview_image(sid, data):
+    # Send preview image to hook
+    hook_url = prompts_map.get(sid, {}).get(PromptKeys.HOOK_URL)
+
+    if sid and hook_url:
+        try:
+            response = requests.post(hook_url, json={"sid": sid}, files={"image": data})
             response.raise_for_status()
         except requests.RequestException as e:
             logging.error(f"Progress extension hook request error: {e}")
@@ -140,22 +142,44 @@ original_send = server.PromptServer.send
 
 
 async def custom_send(self, event, data, sid=None):
-    try:
-        if event == PromptEvent.EXECUTION_START:
-            handle_execution_start(data, sid)
-        elif event == PromptEvent.EXECUTION_CACHED:
-            handle_execution_cached(data)
-        elif event == PromptEvent.EXECUTING:
-            handle_executing(data)
-        elif event == PromptEvent.PROGRESS:
-            handle_progress(data)
-        elif event == PromptEvent.EXECUTED:
-            handle_executed(data)
-    except Exception as e:
-        logging.error(f"Progress extension error: {e}")
+    if sid:
+        try:
+            if event == PromptEvent.EXECUTION_START:
+                handle_execution_start(sid)
+            elif event == PromptEvent.EXECUTION_CACHED:
+                handle_execution_cached(data, sid)
+            elif event == PromptEvent.EXECUTING:
+                handle_executing(data, sid)
+            elif event == PromptEvent.PROGRESS:
+                handle_progress(data, sid)
+            elif event == PromptEvent.EXECUTED:
+                handle_executed(data, sid)
+        except Exception as e:
+            logging.error(f"Progress extension error: {e}")
 
-    # Call the original send method
+    # Call the original method
     await original_send(self, event, data, sid)
 
 
 server.PromptServer.send = custom_send
+
+# ===========================================================
+# Extend server send preview method
+# ===========================================================
+
+original_send_bytes = server.PromptServer.send_bytes
+
+
+async def custom_send_bytes(self, event, data, sid=None):
+    if sid:
+        try:
+            if event == server.BinaryEventTypes.PREVIEW_IMAGE:
+                send_preview_image(sid, self.encode_bytes(event, data))
+        except Exception as e:
+            logging.error(f"Progress extension image preview error: {e}")
+
+    # Call the original method
+    await original_send_bytes(self, event, data, sid)
+
+
+server.PromptServer.send_bytes = custom_send_bytes
